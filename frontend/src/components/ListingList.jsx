18c0596b165acb85d42a4a07af66e4beaf1ecc97
Nyseer Couse <nyseer.couse@gmail.com>
2026-08-21 22:03:56 -0400

    Initial render of table made, still has some bugs and needs styling similar to page

--- frontend/src/components/ListingList.jsx
+++ frontend/src/components/ListingList.jsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
 
+const columns = [
+	{ name: "Name", selector: (row) => row.name, sortable: true },
+	{
+		name: "Price",
+		selector: (row) => row.price.$numberDecimal,
+		sortable: true,
+	},
+	{ name: "Salary", selector: (row) => row.salary, right: true },
+];
 //list of entities
 export default function ListingList() {
 	const [listings, setListings] = useState([]);
@@ -39,46 +49,47 @@ export default function ListingList() {
 		return;
 	}, [listings.length]); //run again if length changes
 
-	if (listings.length === 0) {
-		return (
-			<>
-				<h3>Listings:</h3>
-				<p>No listings found.</p>
-			</>
-		);
-	}
-	if (listings.length === 1) {
-		console.log("length 1");
-		return (
-			<>
-				<h3>Listings:</h3>
-				<div className="listing-item" key={listings[0]._id}>
-					<h2>{listings[0].name}</h2>
-					<br />
-					{/* <p>ID: ${listing._id}</p> */}
-					<p>Price: ${listings[0].price.$numberDecimal}</p>
-					<br />
-					<p>Sold By: {listings[0].sellerName}</p>
-					<br />
-				</div>
-			</>
-		);
-	}
+	return <DataTable columns={columns} data={listings} pagination />;
+	// if (listings.length === 0) {
+	// 	return (
+	// 		<>
+	// 			<h3>Listings:</h3>
+	// 			<p>No listings found.</p>
+	// 		</>
+	// 	);
+	// }
+	// if (listings.length === 1) {
+	// 	console.log("length 1");
+	// 	return (
+	// 		<>
+	// 			<h3>Listings:</h3>
+	// 			<div className="listing-item" key={listings[0]._id}>
+	// 				<h2>{listings[0].name}</h2>
+	// 				<br />
+	// 				{/* <p>ID: ${listing._id}</p> */}
+	// 				<p>Price: ${listings[0].price.$numberDecimal}</p>
+	// 				<br />
+	// 				<p>Sold By: {listings[0].sellerName}</p>
+	// 				<br />
+	// 			</div>
+	// 		</>
+	// 	);
+	// }
 
-	return (
-		<>
-			<h3>Listings:</h3>
-			{listings.map((listing) => (
-				<div className="listing-item" key={listing._id}>
-					<h2>{listing.name}</h2>
-					<br />
-					{/* <p>ID: ${listing._id}</p> */}
-					<p>Price: ${listing.price.$numberDecimal}</p>
-					<br />
-					<p>Sold By: {listing.sellerName}</p>
-					<br />
-				</div>
-			))}
-		</>
-	);
+	// return (
+	// 	<>
+	// 		<h3>Listings:</h3>
+	// 		{listings.map((listing) => (
+	// 			<div className="listing-item" key={listing._id}>
+	// 				<h2>{listing.name}</h2>
+	// 				<br />
+	// 				{/* <p>ID: ${listing._id}</p> */}
+	// 				<p>Price: ${listing.price.$numberDecimal}</p>
+	// 				<br />
+	// 				<p>Sold By: {listing.sellerName}</p>
+	// 				<br />
+	// 			</div>
+	// 		))}
+	// 	</>
+	// );
 }
